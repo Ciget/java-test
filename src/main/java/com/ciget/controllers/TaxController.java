bfbd3cfd2eed85012f56263dec555796233ee8b5
Viktor Bilokin <victor.bilokin@gmail.com>
2025-05-15 00:44:24 -0400

    added openAPI generation

--- src/main/java/com/ciget/controllers/TaxController.java
+++ src/main/java/com/ciget/controllers/TaxController.java
@@ -1,17 +1,22 @@
 package com.ciget.controllers;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import io.swagger.v3.oas.annotations.responses.*;
+import io.swagger.v3.oas.annotations.Operation;
 
-@Slf4j
 @RestController()
 @RequestMapping("/tax")
 public class TaxController {
 
+    @Operation(summary = "Test description for the operation")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successful operation"),
+            @ApiResponse(responseCode = "404", description = "Nothing was found "),
+            @ApiResponse(responseCode = "500", description = "test "),
+    })
     @RequestMapping("/")
     public String index() {
-        log.info("Test logging");
         return "Spring Boot REST API";
     }
 }
